21fbbd94c5884bb695a7d308eb6689ec7b42cbab
DuyMinhED <minhnd2424@ut.edu.vn>
2025-12-01 13:55:22 +0700

    zero

--- app/src/main/java/com/baonhutminh/multifood/ui/components/AppBar.kt
+++ app/src/main/java/com/baonhutminh/multifood/ui/components/AppBar.kt
@@ -23,7 +23,7 @@ import com.baonhutminh.multifood.ui.navigation.Screen
 fun AppTopBar(screen: Screen) {
     Surface(
         color = MaterialTheme.colorScheme.surface, // Trắng (#FFFFFF)
-        contentColor = MaterialTheme.colorScheme.primary, // Đen (#000000)
+        contentColor = MaterialTheme.colorScheme.onSurface, // Đen (#000000)
     ) {
         Row(
             modifier = Modifier
